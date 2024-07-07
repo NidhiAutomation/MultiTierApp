8b49345deed05077e2703827e8da12cd71bf8154
NidhiAutomation <74536761+NidhiAutomation@users.noreply.github.com>
2024-07-07 19:15:31 +0530

    Update installApps.sh

--- installApps.sh
+++ installApps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install apache2 php php-mysqli -y
+sudo apt install mysql-client-core-8.0 -y
 sudo mv /var/www/html/index.html /var/www/html/index.html.bak
 sudo mkdir /var/www/inc
 sudo mv dbinfo.inc /var/www/inc
